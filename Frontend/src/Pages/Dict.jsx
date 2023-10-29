59a9b97fd11e4b85f7ba62b5c5fd92471b8b8586
Robin Sen <robinsen2217@gmail.com>
2023-10-29 23:15:59 +0530

    Slight Tweaking and imports

--- Frontend/src/Pages/Dict.jsx
+++ Frontend/src/Pages/Dict.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
-import Chatbot from '../Components/ChatBot';
+
 
 function Dict() {
   return (
  <>
  <Navbar/>
- <Chatbot/>
+
  </>
   )
 }
