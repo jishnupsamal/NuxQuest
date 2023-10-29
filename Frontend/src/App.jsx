cdf30ff80d99119e48baefee005a5dde270332a7
Jishnu Prasad Samal <81853902+jishnupsamal@users.noreply.github.com>
2023-10-29 12:56:20 +0530

    Update App.jsx
    Signed-off-by: Jishnu Prasad Samal <81853902+jishnupsamal@users.noreply.github.com>

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import Navbar from './Components/NAVBAR.JSX'
+import Navbar from './Components/Navbar.JSX'
 import {
   BrowserRouter as Router,
   Routes,
