a832bee542b1a75082d2e2d5725dcaf966480421
Robin Sen <robinsen2217@gmail.com>
2023-10-31 01:07:34 +0530

    Broadcast page and features complete

--- Frontend/src/Components/Hero.jsx
+++ Frontend/src/Components/Hero.jsx
@@ -4,12 +4,12 @@ import Fade from 'react-reveal/Fade';
 
 function Hero() {
   return (
-    <div className="text-white text-4xl min-[455px]:text-7xl font-extrabold flex flex-col bg-[url('https://thumbs.dreamstime.com/b/hand-drawn-old-vintage-map-fantasy-land-mountains-castles-buildings-coastline-closeup-view-selective-focus-172875866.jpg')] bg-cover bg-center bg-no-repeat justify-center  h-[350px] pb-3 pl-2 py-4 gap-y-2">
+    <div className="text-white text-4xl [@media(min-width:455px)]:text-7xl font-extrabold flex flex-col bg-[url('/heroBg.png')] bg-cover bg-center bg-no-repeat justify-center  h-[350px] pb-3 pl-2 py-4 gap-y-2">
       <Fade cascade>
-        <span className=' text-black text-4xl min-[455px]:text-7xl'>Welcome Adventurer
+        <span className=' text-black'>Welcome Adventurer
 </span>
         </Fade>
-    <div className=" text-red-600 text-2xl min-[455px]:text-5xl ">
+    <div className=" text-red-600 text-2xl [@media(min-width:455px)]:text-5xl ">
           <Typewriter
           
             words={['Whatever you seek.', 'Anything you need.', 'Expectations shall exceed.']}
--- Frontend/src/Pages/Broadcast.jsx
+++ Frontend/src/Pages/Broadcast.jsx
@@ -2,15 +2,24 @@ import Navbar from '../Components/Navbar'
 import React from 'react'
 import { useState } from 'react';
 import axios from 'axios';
+import { useEffect } from 'react';
 import { Input } from "@material-tailwind/react";
 import { Textarea } from "@material-tailwind/react";
 import { Button } from "@material-tailwind/react";
 import Spinner from '../Components/Spinner';
+import {
+  Accordion,
+  AccordionHeader,
+  AccordionBody,
+} from "@material-tailwind/react";
 
 function Broadcast() {
+  const [open, setOpen] = useState(1);
+  // const [alwaysOpen, setAlwaysOpen] = useState(true);
   const [vis, setVis] = useState(0)
   const [errors, setErrors] = useState({})
   const [loading, setLoading] = useState(false)
+  const [aList,setAList]=useState([])
   const [formData, setFormData] = useState({
 
     title: '',
@@ -37,38 +46,48 @@ function Broadcast() {
     if (!formData.content.trim()) {
       validationErrors.content = "This is required"
     } 
-
-    // if (!formData.longDesc.trim()) {
-    //   validationErrors.longDesc = "This is required"
-    // } 
     setErrors(validationErrors)
 
     if (Object.keys(validationErrors).length === 0) {
- 
       setLoading(true)
       axios.post('https://nuxquest.onrender.com/announcement/', { title: formData.title, content: formData.content }).then((res) => {
         console.log(res.status)
-        if (res.status === 200) {
-console.log(res.body)
+        if (res.status === 201) {
 setLoading(false)
-          // axios.get('https://dadacon.onrender.com/user/', {
-          //   headers: {
-          //     Authorization: `Bearer ${token}`
-          //   }
-          // })
-          //   .then((res) => setUser(res.data))
-          //   .catch((err) => console.log(err))
+axios.get('https://nuxquest.onrender.com/announcement/')
+.then((res) =>{setAList(res.data.reverse())
+   console.log(aList)
+  
+})
+ .catch((err) => console.log(err))
         }
       }).catch((errors) => {
         setLoading(false)
         setVis(1)
         setTimeout(() => {
           setVis(0)
-        }, 4000);
+        }, 2000);
       })
     }
 
   }
+  useEffect(() => {
+   axios.get('https://nuxquest.onrender.com/announcement/')
+   .then((res) => {setAList(res.data.reverse())
+     console.log(aList)
+    }
+  
+  )
+    .catch((err) => console.log(err))
+  }, []);
+
+  const handleAlwaysOpen = () => setAlwaysOpen((cur) => !cur);
+  const handleOpen = (value) => setOpen(open === value ? 0 : value);
+  const CUSTOM_ANIMATION = {
+    mount: { scale: 1 },
+    unmount: { scale: 0.9 },
+  };
+   
 
   return (
   <>
@@ -82,19 +101,30 @@ setLoading(false)
     <form  className='flex flex-col justify-evenly mx-auto h-[400px] w-1/2'>
     <Input name='title' onChange={handleChange} variant="outlined" label="Title" />
     {errors.title && <span className='text-red-400'>{errors.title}</span>}
-    {/* <Input name='shortDesc' variant="outlined" onChange={handleChange} label="Short Description" />
-    {errors.email && <span className='text-red-400'>{errors.email}</span>} */}
     <Textarea name='content' size="lg" onChange={handleChange} label="Description" rows={8}/>
     {errors.content && <span className='text-red-400'>{errors.content}</span>}
     <Button color='deep-orange' onClick={handleSubmit} variant="gradient">Submit</Button>
-    {vis ? <span className='text-red-400 block'>Access denied</span> : ''}
+    {vis ? <span className='text-red-400 block'>Broadcast Failed</span> : ''}
     {loading && <Spinner />}
     </form>
   </div>
   <hr
       className="my-12 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-black to-transparent opacity-25 dark:opacity-100"
     />
-    <div>
+    <h1 className='font-bold text-5xl leading-20 mx-auto text-center my-2'>Global Feed</h1>
+    <div className='w-2/3 mx-auto mb-10 border-2 border-solid border-black p-4 bg-yellow-500/50 rounded-lg hover:cursor-[url(/cursorFinger.png),_pointer'>
+ 
+      {aList.length > 0 ? aList.map((elem,ind)=>{
+        return <Accordion animate={CUSTOM_ANIMATION} className='hover:cursor-[url(/cursorPointer.png),default]' key={ind} open={open === elem.id}>
+        <AccordionHeader className='hover:cursor-[url(/cursorFinger.png),_pointer]' onClick={() => handleOpen(elem.id)}>
+          {elem.title}
+        </AccordionHeader>
+        <AccordionBody>
+         {elem.content}
+        </AccordionBody>
+      </Accordion>
+      }): <p className='text-center text-xl text-black'>No global announcements.</p>}
+  
     </div>
   </>
   )
