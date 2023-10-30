ea4e8bb020fe341d9dafcfe4fb45bbeefc36ea92
Jishnu Prasad Samal <jishnuprs20@gmail.com>
2023-10-30 21:30:10 +0530

    feat: added list encyclopedia

---
+++ Frontend/src/Components/EncyclopediaMethod.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import axios from 'axios'
+
+const BASE_URL = import.meta.env.VITE_API_BASE_URL
+
+export const ListEncyclopedia = () => {
+    const API_URL = BASE_URL + '/encyclopedia/'
+    
+    return (
+        <>
+            <h1>List</h1>
+            
+        </>
+    )
+}
+
--- Frontend/src/Pages/Encyclopedia.jsx
+++ Frontend/src/Pages/Encyclopedia.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../Components/Navbar'
+// import { ListEncyclopedia } from '../Components/EncyclopediaMethod'
+import axios from 'axios'
 
+const BASE_URL = import.meta.env.VITE_API_BASE_URL
 
 function Encyclopedia() {
+  const [enc, setEnc] = useState('')
+  const API_URL = BASE_URL + '/encyclopedia/'
+  
+  useEffect(() => {
+    axios.get(API_URL)
+      .then(res => console.log(res))
+      .catch(
+        function (e) {
+          console.log(e)
+        })
+  })
   return (
     <>
- <Navbar/>
+      <Navbar />
+      <button>Add Term</button>
 
- </>
+
+    </>
   )
 }
 
 export default Encyclopedia
+
+
