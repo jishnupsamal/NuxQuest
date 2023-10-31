bafd8b4a3ee7931e22cab97fbec84f5a1a74d181
Robin Sen <robinsen2217@gmail.com>
2023-10-31 22:43:31 +0530

    Added Exalia description

--- Frontend/src/Pages/HomePage.jsx
+++ Frontend/src/Pages/HomePage.jsx
@@ -24,7 +24,7 @@ SetFont('font-Esoteric')
     {/* <Card/> */}
     <div className='p-4 my-5'>
     <h1 className='text-2xl [@media(min-width:460px)]:text-5xl font-bold leading-[60px]'>What is NuxEland?</h1>
-<p className='text-sm [@media(min-width:460px)]:text-base'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo perspiciatis ea laudantium a veniam esse nemo expedita similique nobis ut aspernatur earum, reiciendis eligendi reprehenderit distinctio iste numquam, itaque obcaecati.
+<p className='text-sm [@media(min-width:460px)]:text-base'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo perspiciatis ea laudantium a veniam esse nemo expedita similique nobis ut aspernatur earum, reiciendis eligendi reprehenderit distinctio iste numquam, itaque obcaecati.
 Dolore ipsam dicta consequatur repellat velit itaque, excepturi magnam harum iusto et consequuntur tenetur ratione rem ea. Repellat fugiat non, perspiciatis rerum deserunt sint omnis animi quos saepe quis blanditiis.
 Iure corporis inventore  voluptate recusandae expedita ea nulla corrupti neque in earum esse, inventore, necessitatibus numquam natus! Eveniet quo eius accusantium quam veniam ducimus necessitatibus similique aliquam beatae.
 Voluptatum neque vero nulla minima ad magnam illum sapiente quos amet. Autem nesciunt dolore quo voluptatibus aspernatur laudantium repudiandae eos, delectus ut corporis laboriosam, libero nisi assumenda dolorum, dolorem aliquid.
@@ -38,11 +38,7 @@ Unde consectetur enim odio ad odit molestias iste maiores! Perspiciatis exercita
 Nemo, unde quasi officiis voluptatibus facere, magnam sed voluptas voluptate pariatur ex iste! Ipsam expedita excepturi voluptatibus consectetur ve dignissimos.</p>
     <div>
       <h1 className='text-2xl [@media(min-width:460px)]:text-5xl mt-4 font-bold leading-[60px]'>Dialect of NuxEland: <span className='text-red-500'>Exalia</span></h1>
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam eos saepe veritatis voluptas, voluptatum tenetur ipsa vero quisquam voluptates quasi? Ad unde sit iste cupiditate sed voluptatibus, saepe corrupti. Quis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet doloremque repellat animi temporibus, harum quos dolorem culpa fuga, quibusdam iure quia incidunt, labore nam voluptates cumque soluta enim? Dolorem, nisi?
-      Ratione molestias sapiente animi distinctio accusantium temporibus fugit harum alias maiores beatae minus quibusdam, sint enim aspernatur facere doloribus, et alias?
-      Aperiam ea ducimus molestiae nulla reiciendis.voluptatum.
-      Consectetur porro iste explicabo provident deleniti aliquid, nam esse officiis amet, molestiae quibusdam tempore, sit deserunt totam! Hic ipsum velit, perferendis tempore ratione doloribus eveniet sunt, recusandae adipisci deleniti exercitationem!
-      Deleniti aperiam dolore laborum, hic ullam iusto maxime veritatis voluptatum, nulla explicabo, officia ab exercitationem accusantium assumenda suscipit atque dolor iste eveniet cum voluptate deserunt. Consectetur voluptates dolorem ex reprehenderit?</p>
+      <p>Exalia is the 1000-year old ancient dialect spoken all across NuxEland. It is very easy to read and write and <span className='text-red-500'>Veronica Verma</span> herself learnt it only in two months in her journey to NuxEland. It is was first written by <span className='text-red-500'>Jishnu the Enlightened One</span>. He was born in secluded village in <span className='text-red-500'>Baxlam Kingdom</span>. He developed Exalia at the age of 15 and sought to educate many others to elevate them from conflict. Throughout the years, Exalia has united many communities and is the national language of NuxEland.</p>
       <div className='w-[250px] [@media(min-width:460px)]:w-[400px] mt-3 mx-auto flex flex-col gap-y-3'>
 
       <textarea placeholder='Enter Text' className={`${font} placeholder:text-gray-800 font-bold text-black text-md outline-gray-700 outline p-3   outline-1 rounded-lg block w-full`}  color='deep-orange' rows={8}/>
