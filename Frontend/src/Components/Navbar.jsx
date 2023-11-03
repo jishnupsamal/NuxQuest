e0d01e089a0601d4efd217c1982a157d8137b074
Robin Sen <robinsen2217@gmail.com>
2023-11-03 23:49:38 +0530

    added logo,favicon

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -8,18 +8,18 @@ function Navbar() {
   return (
     <div className=''>
       <header className="text-gray-600 body-font bg-[#9A3B3B] shadow-md shadow-red-900/70 z-10">
-  <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row  items-center">
+  <div className="container mx-auto flex flex-wrap p-2 flex-col md:flex-row  items-center">
     <Link to={'/'} className="hover:cursor-[url(/cursorFinger.png),_pointer] flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+      <img src="/nuxquestlogo.webp" alt="nuxquestlogo" className='w-20' />
       {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-yellow-500 rounded-full" viewBox="0 0 24 24">
         <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
       </svg> */}
-      <span className=" text-xl text-transparent bg-clip-text bg-gradient-to-r from-green-500 to-yellow-600 via-zinc-800 font-semibold">NuxQuest</span>
+      {/* <span className=" text-xl text-transparent bg-clip-text bg-gradient-to-r from-green-500 to-yellow-600 via-zinc-800 font-semibold">NuxQuest</span> */}
     </Link>
     <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l gap-y-2 md:border-[#DE9B72] text-white flex flex-wrap items-center text-base justify-center">
     <Anchor href={'/home'} content={'Home'} exStyle={"mr-5"}/>
       <Anchor href={'/encyclopedia'} content={'Encyclopedia'} exStyle={"mr-5"}/>
            <Anchor href={'/broadcast'} exStyle={"mr-5"} content={'Broadcast'}/> 
-                {/* <Anchor href={'/dict'} exStyle={"mr-5"} content={'Dictionary'}/> */}
     </nav>
  
   </div>
--- Frontend/src/Pages/Landing.jsx
+++ Frontend/src/Pages/Landing.jsx
@@ -3,17 +3,20 @@ import Anchor from '../Components/Link'
 
 function Landing() {
   return (
-    <div className="overflow-hidden h-[100vh] before:bg-[url('/landingBg.jpg')] before:absolute before:top-0 before:bg-no-repeat before:bg-cover before:bg-center before:bottom-0 before:right-0 before:left-0  flex justify-center items-center before:z-2 before:h-[100vh] before:w-[100vw] before:blur-[5px]">
-      <div className='[@media(min-width:727px)]:h-3/5 h-2/5 w-3/5 bg-gray-800/80 z-10 shadow-2xl shadow-black flex flex-col justify-evenly'>
-        <div className='text-center border-y-2 border-white w-3/5 [@media(min-width:455px)]:text-3xl [@media(min-width:727px)]:text-5xl py-3 font-extrabold mx-auto'>
-            <p className='text-yellow-400'>Welcome to
-            </p>
-            <p className='font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-red-600 to-green-600 via-gray-700 leading-loose'>
-                NuxQuest
-            </p>
-        </div>
-            <Anchor href={'/home'} content={'Enter'} exStyle={'px-4 text-white font-bold bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500  mx-auto p-2 rounded-lg'}/>
-      </div>
+    // <div className="overflow-hidden h-[100vh] before:bg-[url('/landingBg.jpg')] before:absolute before:top-0 before:bg-no-repeat before:bg-cover before:bg-center before:bottom-0 before:right-0 before:left-0  flex justify-center items-center before:z-2 before:h-[100vh] before:w-[100vw] before:blur-[5px]">
+    //   <div className='[@media(min-width:727px)]:h-3/5 h-2/5 w-3/5 bg-gray-800/80 z-10 shadow-2xl shadow-black flex flex-col justify-evenly'>
+    //     <div className='text-center border-y-2 border-white w-3/5 [@media(min-width:455px)]:text-3xl [@media(min-width:727px)]:text-5xl py-3 font-extrabold mx-auto'>
+    //         <p className='text-yellow-400'>Welcome to
+    //         </p>
+    //         <p className='font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-red-600 to-green-600 via-gray-700 leading-loose'>
+    //             NuxQuest
+    //         </p>
+    //     </div>
+    //   </div>
+    // </div>
+    <div className="overflow-hidden h-[100vh] w-full before:bg-[url('/landingBg.jpg')] before:absolute before:top-0 before:bg-no-repeat before:bg-cover before:bg-center before:bottom-0 before:right-0 before:left-0  flex justify-center items-center before:z-2 before:h-[100vh] before:w-[100vw] before:blur-[3px]">
+      <img src="/public/nuxquestlogo.webp" alt="nuxquestlogo" className='z-10 [@media(min-width:731px)]:h-[700px] ' />
+      <Anchor href={'/home'} content={'Enter'} exStyle={'px-4 text-white font-bold bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500  mx-auto p-2 rounded-lg z-[11] self-center absolute'}/>
     </div>
   )
 }
